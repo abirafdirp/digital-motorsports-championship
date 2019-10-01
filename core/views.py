0838f9743270c089af3abc65eadde3a5e56c1741
Abirafdi Raditya Putra <abirafdiraditya@gmail.com>
2019-10-01 22:15:35 +0700

    Add request obj to context

--- core/views.py
+++ core/views.py
@@ -7,8 +7,18 @@ from core import forms
 class RegistrationSucceedTemplateView(TemplateView):
     template_name = 'registration_succeed.html'
 
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['request'] = self.request
+        return context
+
 
 class IndexCreateView(CreateView):
     template_name = 'index.html'
     form_class = forms.UserProfileModelForm
     success_url = reverse_lazy('registration_succeed')
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['request'] = self.request
+        return context
